673ce1be17b0ca202da3e2f9f92e239dea7ba8fa
iqramqra <masrooriqra@gmail.com>
2020-02-24 14:22:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(resp => resp.json())
+.then(json => renderImg(json.message));
+
+
+function renderImg(image){
+    const img = document.querySelector("#dog-image-container");
+    image.forEach(e => {
+        let element = document.createElement("img");
+        element.src = e;
+        img.appendChild(element);
+    });
+}
