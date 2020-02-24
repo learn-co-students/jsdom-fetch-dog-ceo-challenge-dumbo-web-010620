e9d7dc6ac0134f79ac97e4ab9848aa9013cf12f1
Gregory Diaz <gregdiaz8624@yahoo.com>
2020-02-24 12:58:18 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
-console.log('%c HI', 'color: firebrick')
+
+function fetchDogs() {
+    return fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(resp => resp.json())
+    .then(json => renderDogs(json.message)); //turning it into an array here
+}
+
+function renderDogs(array) { //basically the array we fetched on line 4
+    //find parent
+    const dogContainer = document.querySelector('#dog-image-container')
+    //need an element to attach the array 
+    //iterate through an array
+    array.forEach(dog => {
+        let img = document.createElement('img');
+        img.src = dog;
+        dogContainer.appendChild(img)
+    })
+}
+
+function fetchBreed(){
+    return fetch('https://dog.ceo/api/breeds/list/all')
+    .then(resp => resp.json())
+    .then(json => renderBreed(json.message));
+}
+
+function renderBreed(json){
+
+    //iterate thgrough an array
+    const breedUl = document.querySelector('#dog-breeds')
+    let li = document.createElement('li')
+    const breedList = Object.keys(json)
+    breedList.forEach(breed => {
+        let li = document.createElement('li')
+        li.textContent = breed 
+        breedUl.appendChild(li)
+        li.addEventListener("click", event => {
+            li.style.color = "purple"
+        })    
+    })
+
+    const breedDropDown = document.querySelector("#breed-dropdown")
+
+    breedDropDown.addEventListener("change", e =>{
+        let userInput = e.target.value
+        let filteredList = breedList.filter(breed => userInput === breed[0])
+        breedUl.innerHTML =  ""
+
+        filteredList.forEach(breed => {
+            const li = document.createElement('li')
+            li.textContent = breed
+            breedUl.appendChild(li)
+        })
+    })
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchDogs()
+    fetchBreed()
+})
